d5d994db94b6bb719def547f2e7c82edfb394328
vadim2030 <dbunos@mail.ru>
2023-08-05 22:38:29 +0300

    update brain-games

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const cli = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default cli;
