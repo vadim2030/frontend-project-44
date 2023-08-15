2c61b05b1f7bd901401e8cf58dc21bfbd0a2d65e
vadim2030 <dbunos@mail.ru>
2023-08-15 19:10:56 +0300

    fix primeGame

--- src/games/runPrimeGame.js
+++ src/games/runPrimeGame.js
@@ -4,11 +4,11 @@ import runEngineGame from '../index.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 2) return true;
+  if (num < 2) return false;
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
-  return num !== 1;
+  return true;
 };
 
 const getRoundData = () => {
