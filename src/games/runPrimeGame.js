451001351d1c314eb191480c8a8448bbe8756f66
vadim2030 <dbunos@mail.ru>
2023-08-15 19:06:00 +0300

    fix primeGame

--- src/games/runPrimeGame.js
+++ src/games/runPrimeGame.js
@@ -5,7 +5,7 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
   if (num < 2) return true;
-  for (let i = 2; i < num / 2; i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return num !== 1;
