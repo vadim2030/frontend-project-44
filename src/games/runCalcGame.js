6f7b82ae06041707c5e63db07d7a86647527af21
vadim2030 <dbunos@mail.ru>
2023-08-16 21:03:19 +0300

    fix calcGame

--- src/games/runCalcGame.js
+++ src/games/runCalcGame.js
@@ -6,8 +6,6 @@ const rule = 'What is the result of the expression?';
 
 const signs = ['+', '*', '-'];
 
-const getSignRandom = () => signs[getRandomInRange(0, signs.length - 1)];
-
 const runCalculator = (num1, num2, operand) => {
   switch (operand) {
     case '-':
@@ -21,11 +19,9 @@ const runCalculator = (num1, num2, operand) => {
   }
 };
 
-const getNumbers = () => [getRandomInRange(), getRandomInRange()].sort((a, b) => b - a);
-
 const getRoundData = () => {
-  const [firstNum, secondNum] = getNumbers();
-  const signRandom = getSignRandom();
+  const [firstNum, secondNum] = [getRandomInRange(), getRandomInRange()].sort((a, b) => b - a);
+  const signRandom = signs[getRandomInRange(0, signs.length - 1)];
   const questionStart = `${firstNum} ${signRandom} ${secondNum}`;
   const question = `Question: ${questionStart}`;
   const answer = String(runCalculator(firstNum, secondNum, signRandom));
